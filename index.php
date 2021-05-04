54e703fbd8c0ab464984b383a5aae8d018e45240
=wajuma <=meltusjuma5@gmail.com>
2021-05-04 20:09:47 +0300

    html boilerplate

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en-KE" dir="ltr">
+	<head>
+		<title>chat-App</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+	</head>
+	<body>
+		
+	</body>
+</html>
