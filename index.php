eaa6ea5893c2bcd4dce20668b2042d64d047c86d
=wajuma <=meltusjuma5@gmail.com>
2021-05-05 09:34:15 +0300

    pull upstream

--- index.php
+++ index.php
@@ -6,6 +6,26 @@
 		<meta name="viewport" content="width=device-width">
 	</head>
 	<body>
-		
+		<?php
+			require './class.config.php';
+
+			class Chats extends DbConfig{
+				protected function getChats(){
+					$sql ="SELECT * FROM chats";
+					$conn =$this->connect();
+					if($conn){
+						$query =$conn->query($sql);
+						$num_rows =$query->num_rows;
+						if($num_rows > 0){
+							echo $num_rows;
+						}
+					}else{
+						echo 'err';
+					}
+				}
+			}
+			$chats =new Chats();
+			$chats->getChats();
+		?>
 	</body>
 </html>
