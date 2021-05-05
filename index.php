b80f1692f7fc62868825c3a7e6939ff0edf29ee5
=wajuma <=meltusjuma5@gmail.com>
2021-05-05 11:22:17 +0300

    add navbar component

--- index.php
+++ index.php
@@ -9,7 +9,19 @@
 		<link rel="stylesheet" href="./custom.css">
 	</head>
 	<body>
-		
+		<nav class="navbar navbar-expand-lg navbar-dark bg-purple">
+		  <a class="navbar-brand" href="#">Chat-App</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		    <div class="navbar-nav ml-auto pr-5">
+		      <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+		      <a class="nav-link" href="#">Inbox</a>
+		      <a class="nav-link" href="#">Profile</a>
+		    </div>
+		  </div>
+		</nav>
 		<!--- jquery, popper, bootstrap & app scripts  --->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" ></script>
