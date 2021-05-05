e579e284232086da2236ad9126259164bec474a4
=wajuma <=meltusjuma5@gmail.com>
2021-05-05 10:02:33 +0300

    set custom defaults

--- index.php
+++ index.php
@@ -4,28 +4,16 @@
 		<title>chat-App</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width">
+		<!--- bootstrap, fonts, icons & custom css ---->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+		<link rel="stylesheet" href="custom.css">
 	</head>
 	<body>
-		<?php
-			require './class.config.php';
-
-			class Chats extends DbConfig{
-				protected function getChats(){
-					$sql ="SELECT * FROM chats";
-					$conn =$this->connect();
-					if($conn){
-						$query =$conn->query($sql);
-						$num_rows =$query->num_rows;
-						if($num_rows > 0){
-							echo $num_rows;
-						}
-					}else{
-						echo 'err';
-					}
-				}
-			}
-			$chats =new Chats();
-			$chats->getChats();
-		?>
+		
+		<!--- jquery, popper, bootstrap & app scripts  --->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" ></script>
+    	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+    	<script src="./app.js"></script>
 	</body>
 </html>
